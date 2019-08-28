12bc4729f0d5dd0f958b7500bba8236e22ddb8a4
wxxlouisa <569937952@qq.com>
2019-08-28 10:08:01 +0800

    Create GlobFiles.py

---
+++ GlobFiles.py
@@ -0,0 +1,18 @@
+import feat as featModule
+import glob
+import numpy as np
+import os.path as osp
+from collections import OrderedDict
+import mmcv
+
+fnList = glob.glob("/root/whome/genreClass/arxiv/GTZAN/wav/*.wav")
+featDict = OrderedDict()
+pbar = mmcv.ProgressBar(len(fnList))
+for fn in fnList:
+    sb = featModule.lowLevelExtractor(fn)
+    sb0 = sb[0]['lowlevel.spectral_contrast_coeffs.mean']
+    sb1 = sb[0]['lowlevel.spectral_contrast_coeffs.stdev']
+    feat = np.concatenate([sb0, sb1])
+    id_ = osp.basename(fn)
+    featDict.update({id_:feat})
+    pbar.update()
